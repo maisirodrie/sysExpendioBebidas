7c242e74b49d807cd7cc7e7098cb60dfd36f1d5c
Elias Daniel Emanuele <44514963+emanueleelias@users.noreply.github.com>
2024-04-30 11:48:27 -0300

    Cambio de url api en front

--- Rothda/src/api/axios.js
+++ Rothda/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'http://138.117.77.148:3005/api',
     withCredentials:true
 })
 
