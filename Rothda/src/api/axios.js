33802a02551fc77ef95b9b6640a5adb5b1829741
maisi-r <maisi8.5@gmail.com>
2024-05-03 13:20:08 -0300

    Se arreglo el tipo fecha que traia un dia antes

--- Rothda/src/api/axios.js
+++ Rothda/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://138.117.77.148:3005/api',
+    // baseURL: 'https://138.117.77.148:3005/api',
+    baseURL: 'http://localhost:3000/api',
     withCredentials:true
 })
 
--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import app from "./app.js"
 import { connectDB } from "./db.js"
 
 connectDB()
-app.listen (3005)
-console.log('Server on port', 3005)
+app.listen (3000)
+console.log('Server on port', 3000)
