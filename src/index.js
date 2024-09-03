c9f27ee45d4d063caf880a8406785b8121ad65a0
maisi-r <maisi8.5@gmail.com>
2024-09-03 11:15:31 -0300

    Prueba de puertos

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import app from "./app.js"
 import { connectDB } from "./db.js"
 
 connectDB()
-app.listen (3007)
-console.log('Server on port', 3007)
+app.listen (3001)
+console.log('Server on port', 3001)
