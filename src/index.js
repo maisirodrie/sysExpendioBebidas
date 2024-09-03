287794d19314bbe04681391f260e3466343c1af8
maisi-r <maisi8.5@gmail.com>
2024-09-03 13:19:59 -0300

    servidor pa 3006

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import app from "./app.js"
 import { connectDB } from "./db.js"
 
 connectDB()
-app.listen (3001)
-console.log('Server on port', 3001)
+app.listen (3006)
+console.log('Server on port', 3006)
