939964d6c64e6dbc9dd365a0810b6bafdbbd1010
maisi-r <maisi8.5@gmail.com>
2024-09-05 12:14:32 -0300

    cambio url axio y app falta tabla

--- GestionArchivo/src/api/axios.js
+++ GestionArchivo/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    // baseURL: 'https://www.api.misiones.gov.ar:3006/api', 
-    baseURL: 'http://localhost:3006/api',
+    baseURL: 'https://www.apiarchivo.misiones.gov.ar/api', 
+    // baseURL: 'http://localhost:3006/api',
     withCredentials:true
 })
 
