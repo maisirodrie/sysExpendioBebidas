87f58674bdb1ecd0fac63850e5a9218b0d0f37f4
maisi-r <maisi8.5@gmail.com>
2024-09-11 10:34:36 -0300

    login prueba

--- GestionArchivo/src/pages/LoginPage.jsx
+++ GestionArchivo/src/pages/LoginPage.jsx
@@ -1,28 +1,26 @@
-import React, { useEffect } from 'react';
-import { useForm } from 'react-hook-form';
-import { useAuth } from '../context/AuthContext';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { useAuth } from "../context/AuthContext";
+import { Link, useNavigate } from "react-router-dom";
 import "./login.css";
 
 function LoginPage() {
-  const { register, handleSubmit, formState: { errors } } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const { signin, errors: signinErrors, isAuthenticated, user } = useAuth();
   const navigate = useNavigate();
 
   const onSubmit = handleSubmit((data) => {
     signin(data);
   });
-
   useEffect(() => {
     if (isAuthenticated) {
-      // Redirige a la página de tareas para roles específicos
-      if (user.role === 'admin' || user.role === 'user') {
-        navigate("/task");
-      } else if (user.role === 'boss' || user.role === 'viewer') {
-        navigate("/task"); // Puedes ajustar esto según sea necesario
-      }
+      navigate("/task");
     }
-  }, [isAuthenticated, user, navigate]);
+  }, [isAuthenticated, navigate]);
 
   return (
     <section>
@@ -30,43 +28,55 @@ function LoginPage() {
         className="bg-cover bg-no-repeat py-10 text-white relative flex flex-col justify-center items-center"
         style={{
           backgroundImage: `url('./fondos/fondo.jpg')`,
-          backgroundPosition: 'center bottom', // Centra horizontalmente y alinea en la parte inferior
-          padding: '10vw 2rem', // Padding ajustable en unidades relativas (viewport width)
+          backgroundPosition: "center bottom", // Centra horizontalmente y alinea en la parte inferior
+          padding: "10vw 2rem", // Padding ajustable en unidades relativas (viewport width)
         }}
       >
-        <div className="bg-dark-overlay absolute inset-0"></div> {/* Superposición de color oscuro */}
+        <div className="bg-dark-overlay absolute inset-0"></div>{" "}
+        {/* Superposición de color oscuro */}
         <div className="container mx-auto relative z-10 text-center">
           <div className=" flex items-center justify-center">
             <div className="bg-gray-200 shadow-md max-w-md w-full p-10 rounded-md">
-            {Array.isArray(signinErrors) && signinErrors.map((error, i) => (
-  <div className='bg-red-500 p-2 text-white' key={i}>
-    {error}
-  </div>
-))}
+              {Array.isArray(signinErrors) &&
+                signinErrors.map((error, i) => (
+                  <div className="bg-red-500 p-2 text-white" key={i}>
+                    {error}
+                  </div>
+                ))}
 
-              <h1 className='text-2xl font-bold text-black'>Acceso</h1>
+              <h1 className="text-2xl font-bold text-black">Acceso</h1>
               <form onSubmit={onSubmit}>
-              <label htmlFor="username" className="block text-sm font-medium text-black text-left">
-                                Usuario
-                            </label>
+                <label
+                  htmlFor="username"
+                  className="block text-sm font-medium text-black text-left"
+                >
+                  Usuario
+                </label>
                 <input
                   type="text"
                   {...register("username", { required: true })}
-                  className='w-full bg-white text-gray-600 px-4 py-2 rounded-md my-2 border border-gray-300'
-                  placeholder='Usuario'
+                  className="w-full bg-white text-gray-600 px-4 py-2 rounded-md my-2 border border-gray-300"
+                  placeholder="Usuario"
                 />
-                {errors.username && (<p className='text-red-500'>Usuario es requerido</p>)}
-                
-                <label htmlFor="password" className="block text-sm font-medium text-black text-left">
-                                Contraseña
-                            </label>
+                {errors.username && (
+                  <p className="text-red-500">Usuario es requerido</p>
+                )}
+
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium text-black text-left"
+                >
+                  Contraseña
+                </label>
                 <input
                   type="password"
                   {...register("password", { required: true })}
-                  className='w-full bg-white text-gray-600 px-4 py-2 rounded-md my-2 border border-gray-300'
-                  placeholder='Contraseña'
+                  className="w-full bg-white text-gray-600 px-4 py-2 rounded-md my-2 border border-gray-300"
+                  placeholder="Contraseña"
                 />
-                {errors.password && (<p className='text-red-500'>Contraseña es requerida</p>)}
+                {errors.password && (
+                  <p className="text-red-500">Contraseña es requerida</p>
+                )}
                 <button className="custom-button">Iniciar Sesión</button>
               </form>
               {/* <p className="flex gap-x-2 justify-between text-black">
