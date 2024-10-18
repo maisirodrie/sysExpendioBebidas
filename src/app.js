14ec3a058a8133ae9d7b4da8e9b01bc2a0b69f07
maisi-r <maisi8.5@gmail.com>
2024-10-18 20:59:51 -0300

    cambio de api nuevamente

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const app = express();
 // }));
 
 app.use(cors({
-    origin: 'https://www.expendiobebidas.misiones.gov.ar/',
+    origin: 'https://www.expendiobebidas.misiones.gov.ar',
     credentials: true,
 })); 
 
